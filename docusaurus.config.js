3954f1ed09099f2e153eeaa5e20f7a92a27e3e33
Christopher Espiritu <christopheredrian@gmail.com>
2023-10-01 08:36:23 -0400

    trailing slash updates per doc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'christopheredrian', // Usually your GitHub org/user name.
   projectName: 'docusaurus-playground', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
